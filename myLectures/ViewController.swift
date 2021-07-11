1f10a59e7aeb46603c12fee4eb403444eb03c1bd
3rnestocs <3rnestocs@gmail.com>
2021-07-11 18:26:28 -0400

    Initial project setup

---
+++ myLectures/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  myLectures
+//
+//  Created by Ernesto Jose Contreras Lopez on 7/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
